126f0264f476338d89e8d69b663ac0ae757312ae
zenimaniprima <sidnazayn11@gmail.com>
2023-10-17 16:58:29 +0700

    update again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
-})
+    applyBaseStyles: false
+  }), compress(), vue()]
+});
