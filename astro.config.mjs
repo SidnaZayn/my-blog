459c326786297a25547ff3e1a865cb72b2d15d58
SidnaZayn <sidnazayn11@gmail.com>
2024-01-05 16:57:08 +0700

    update default hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
+import icon from "astro-icon";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), tailwind({
+  integrations: [icon(), mdx(), tailwind({
     applyBaseStyles: false
   }), compress(), vue()]
 });
---
+++ src/components/vue/AsideNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-slate-900 flex items-center justify-end md:justify-center">
+    <div class="grid grid-cols-1 gap-y-8">
+      <a href="https://github.com/sidnazayn" title="Go to the GitHub page">
+        <Icon icon="ri:github-fill" class="h-8 w-8" />
+      </a>
+      <a href="https://www.linkedin.com/in/sidna-muhammad-zen-1a0898180/" title="Go to the GitHub page">
+        <Icon icon="ri:linkedin-box-fill" class="h-8 w-8" />
+      </a>
+      <a href="https://twitter.com/talkativepeep" title="Go to the GitHub page">
+        <Icon icon="ri:twitter-x-fill" class="h-8 w-8" />
+      </a>
+      <a href="https://medium.com/@sidnazayn11" title="Go to the GitHub page">
+        <Icon icon="ri:medium-fill" class="h-8 w-8" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Icon } from "@iconify/vue";
+
+const sidebar = ref('this is side bar')
+</script>
